9fab70d0a26698d4e880dd8d520f3d210ad94605
heiqiaoxiang <qiaoxianghei@gmail.com>
2016-09-16 13:13:38 +0800

    commit test1

---
+++ src/Pow276.java
@@ -0,0 +1,16 @@
+/**
+ * Created by parallel on 9/16/16.
+ */
+public class Pow276 {
+    /**
+     * Problem:
+     * Implement pow(x, n).
+     * This is a great example to illustrate how to solve a problem during a technical
+     * interview. The first and second solution exceeds time limit; the third and fourth are
+     * accepted.
+     * @param args
+     */
+    public void main(String[] args){
+
+    }
+}
