d926d53911e3af75a533ee53856eb4f28f8e0517
Coco_parallel <804383999@qq.com>
2016-09-16 13:22:48 +0800

    commit test1

--- src/Pow276.java
+++ src/Pow276.java
@@ -10,7 +10,29 @@ public class Pow276 {
      * accepted.
      * @param args
      */
-    public void main(String[] args){
-
+    public static void main(String[] args){
+//        test
+        System.out.println(pow(2,2));
+        System.out.println(pow(2,-2));
+        System.out.println(pow(0.2,0));
+        System.out.println(pow(-2,3));
+        System.out.println(pow(-2,2));
+    }
+    // time O(logn) n>=0
+    public static double power(double x, int n){
+        if(n == 0) return 1;
+        double v = power(x, n/2);
+        if(n%2 == 0) {
+            return v*v;
+        } else {
+            return x*v*v;
+        }
+    }
+    public static double pow(double x, int n){
+        if(n<0){
+            return 1.0/power(x,-n);
+        }else {
+            return power(x,n);
+        }
     }
 }
